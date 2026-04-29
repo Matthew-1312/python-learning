fb6628c74e153bf401e2fad0e62ad1ca9a96f245
Matthew <kapi4443@gmail.com>
2026-04-29 20:17:46 +0200

    Update day_001.py

--- day_001.py
+++ day_001.py
@@ -1,4 +1,7 @@
-# Write your code below this line 👇
+# Day 1 — Band Name Generator
+# Angela Yu's 100 Days of Code
+# Asks user for city + pet name and suggests a band name.
+
 print("Brand name generator")
 city = input("What city u live?\n")
 pet = input("What name of ur first pet?\n")
