f1645eb77c37f1250bea92d0e0f2cd7f9f0d6f07
Matthew <kapi4443@gmail.com>
2026-04-29 20:13:50 +0200

    code of day 1

---
+++ day_001.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Brand name generator")
+city = input("What city u live?\n")
+pet = input("What name of ur first pet?\n")
+print("Your brand name could be " + city + " " + pet)
