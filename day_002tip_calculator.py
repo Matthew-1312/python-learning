85eb49e8639f6aa9e26b1424fb9cb244d6bec721
Matthew <kapi4443@gmail.com>
2026-05-02 01:37:08 +0200

    Update and rename day_002tip_calculator to day_002tip_calculator.py

---
+++ day_002tip_calculator.py
@@ -0,0 +1,10 @@
+# Day 2 Alysa'a 
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+total_with_tip = bill * (1 + tip / 100)
+each_pays = total_with_tip / people
+
+print(f"Each person should pay: ${each_pays:.2f}")
