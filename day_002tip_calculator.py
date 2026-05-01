84e566e9b41acd23ed49b1f70527f36d85f49126
Matthew <kapi4443@gmail.com>
2026-05-02 01:38:45 +0200

    Update day_002tip_calculator.py

--- day_002tip_calculator.py
+++ day_002tip_calculator.py
@@ -1,4 +1,7 @@
-# Day 2 Alysa'a 
+# Day 2 — Tip Caltulator
+# Angela Yu's 100 Days of Code
+# Asks user for city, bill and people and calculate how much they must pay
+
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
